d94b07d8c1464f8bbca6adc8b3ea4bd670d657e5
fchery87 <frantzchery@hotmail.com>
2026-03-27 18:26:12 -0400

    chore: add migration for new transition types

---
+++ drizzle/migrations/0003_bright_glorian.sql
@@ -0,0 +1,3 @@
+ALTER TYPE "public"."transition_type" ADD VALUE 'blur';--> statement-breakpoint
+ALTER TYPE "public"."transition_type" ADD VALUE 'dip-to-black';--> statement-breakpoint
+ALTER TYPE "public"."transition_type" ADD VALUE 'dip-to-white';
